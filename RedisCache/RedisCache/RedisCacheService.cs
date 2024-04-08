1bc9f8decfb914d74a9c6e77929a40ceca1ff1b2
weber.hsu <weber.hsu@titansoft.com.sg>
2024-04-08 22:29:33 +0800

    create interface and config

---
+++ RedisCache/RedisCache/RedisCacheService.cs
@@ -0,0 +1,49 @@
+﻿using StackExchange.Redis;
+
+namespace RedisCache;
+
+public class RedisCacheService(RedisConfig redisConfig): ICacheService
+{
+    private readonly string _keyPostfix = redisConfig.KeyPostfix;
+    private readonly IDatabase _redisDb = redisConfig.GetConnection();
+
+    public void Add<TValue>(string key, TValue value, TimeSpan expireTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TValue Get<TValue>(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryGet<TValue>(string key, out TValue? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TValue? GetOrCreate<TValue>(string key, Func<TValue> getValueFunc, TimeSpan expireTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TValue> GetOrCreate<TValue>(string key, Func<Task<TValue>> getValueFunc, TimeSpan expireTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TValue> GetAsync<TValue>(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Task> AddAsync<TValue>(string key, TValue value, TimeSpan expireTime)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ RedisCache/RedisCache/RedisConfig.cs
@@ -0,0 +1,63 @@
+﻿using StackExchange.Redis;
+using StackExchange.Redis.Extensions.Core.Configuration;
+
+namespace RedisCache;
+
+public class RedisConfig
+{
+    private readonly ConnectionMultiplexer _connection;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="connectionString">Redis Connection String.</param>
+    /// <param name="dbNumber">Redis database number, can be 0 to 15</param>
+    /// <param name="keyPostfix">Postfix to prevent key duplicated with other project. For example if set this value to '_cc' for cache key 'my_cache_key', the real cache key will be 'my_cache_key_cc'</param>
+    public RedisConfig(string connectionString, int dbNumber = 1, string keyPostfix = "")
+    {
+        KeyPostfix = keyPostfix;
+        _connection = ConnectionMultiplexer.Connect($"{connectionString},defaultDatabase={dbNumber}");
+    }
+
+    private RedisConfig(ConfigurationOptions options, string keyPostfix = "")
+    {
+        KeyPostfix = keyPostfix;
+        _connection = ConnectionMultiplexer.Connect(options);
+    }
+
+    public string KeyPostfix { get; }
+
+    public IDatabase GetConnection()
+    {
+        return _connection.GetDatabase();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="dbNumber">Redis database number, can be 0 to 15</param>
+    /// <param name="keyPostfix">Postfix to prevent key duplicated with other project. For example if set this value to '_cc' for cache key 'my_cache_key', the real cache key will be 'my_cache_key_cc'</param>
+    /// <returns></returns>
+    public static RedisConfig UseSealCluster(string password, int dbNumber = 1, string keyPostfix = "")
+    {
+        var redisConfiguration = new RedisConfiguration()
+        {
+            Hosts = new RedisHost[]
+            {
+                new()
+                {
+                    Host = "localhost",
+                    Port = 6379
+                },
+            },
+            AbortOnConnectFail = false,
+            Database = dbNumber,
+            ConnectTimeout = 5000,
+            Password = password,
+            SyncTimeout = 5000
+        };
+
+        return new RedisConfig(redisConfiguration.ConfigurationOptions, keyPostfix);
+    }
+
+}
